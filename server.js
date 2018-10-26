1402124c75859448f72b8512b8227eb2246c08d4
신현수 <pmjahy@gmail.com>
2018-10-26 16:10:50 +0900

    20181026.1610

--- server.js
+++ server.js
@@ -10,20 +10,29 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-
+//////////////////////////////////////////////////////////
 
 io.on('connection', (socket) => {
+  
   console.log('a user connected');
+  
+  socket.on('roomEnter', (msg) => {
+    //io.emit('chat message', msg);
+    socket.join(msg);
+    console.log("user enter room :: "+msg);
+  });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('chatMsg', (msg) => {
+    
+    console.log("chatMsg"+msg);
+  
   });
 
   socket.on('disconnect', () => {
   console.log('user disconnected');
   });
 });
-
+//////////////////////////////////////////////////////////
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 })
