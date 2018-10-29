bbe8201623431a9175e786eb43df24d640701f03
신현수 <pmjahy@gmail.com>
2018-10-29 18:47:28 +0900

    20181029.0647

--- server.js
+++ server.js
@@ -11,18 +11,45 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 //////////////////////////////////////////////////////////
+var firstTurn="black";
+var listBoard=new Map();
+
 
 io.on('connection', (socket) => {
 
   console.log('a user connected');
 
   socket.on('roomEnter', (msg) => {
-
+    //msg is roomName;
     socket.join(msg);
     var room = io.sockets.adapter.rooms[msg];
+    console.log("room test :: "+room);
+    var roomLength = room.length;
+
+    var data;
+    if(roomLength==1){
+        data={"userColor":"white"};
+        socket.emit("roomEnter",data);
+    }
+    else if(roomLength==2){
+        data={"userColor":"black"};
+        socket.emit("roomEnter",data);
+
+    //game start send firstTurn;
+        //create new board;
+        listBoard.set(msg,createBoard());
+
+        data={"currentTurn":firstTurn};
+        io.to(msg).emit("gameStart",data);
+    }
+    else {
+        data={"userColor":"none"};
+        socket.emit("roomEnter",data);
+    }
+
 
     console.log("user enter room :: "+msg);
-    console.log("room users Num : "+   room.length);
+    console.log("room users Num : "+  roomLength);
 
   });
 
@@ -43,6 +70,29 @@ io.on('connection', (socket) => {
   console.log('user disconnected');
   });
 });
+
+////////////////////////gameManage//////////////////////////////////
+
+  function createBoard(roomName){
+    var board=[0,0,0,0,0,0,0,0,
+               0,0,0,0,0,0,0,0,
+               0,0,0,0,0,0,0,0,
+               0,0,0,1,2,0,0,0,
+               0,0,0,2,1,0,0,0,
+               0,0,0,0,0,0,0,0,
+               0,0,0,0,0,0,0,0,
+               0,0,0,0,0,0,0,0];
+    return board;
+
+  }
+
+  fuction searchDropablePos(board){
+
+
+  }
+  fuction checkBoard(board){
+
+  }
 //////////////////////////////////////////////////////////
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
