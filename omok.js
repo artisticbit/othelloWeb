b4f7e1697e24035958f40ddf1116544e32975879
신현수 <pmjahy@gmail.com>
2018-12-27 18:37:09 +0900

    33

--- omok.js
+++ omok.js
@@ -61,13 +61,23 @@ module.exports=function(io){
 
     socket.on('dropStone', (data) => {
 
+        let doubleThreeFlag=false;
+
         let currentBoard=listBoard.get(data.roomName);
 
         let posIndex=data.pos.x+data.pos.y*15;
         currentBoard[posIndex]=data.dropColor;
 
-
         let nextTurnColor=data.dropColor==1?2:1;
+
+        //33체크
+        let threeFlag=checkThree(currentBoard,data.pos,data.dropColor);
+         if(threeFlag){
+           doubleThreeFlag=true;
+           currentBoard[posIndex]=0;
+           let nextTurnColor=data.dropColor==1?2:1;
+         }
+        //
 
         //listBoard.set(data.roomName,currentBoard);
 
@@ -78,6 +88,13 @@ module.exports=function(io){
 
         io.to(data.roomName).emit('dropStone',nextData);
         //console.log("dropStone"+"x :"+data.pos.x+" y: "+data.pos.y);
+
+        if(doubleThreeFlag){
+          let message="쌍3ㄴㄴ"
+          io.to(data.roomName).emit('doubleThree',{"message":message});
+        }
+
+
         //종료판정
         var checkFlag = checkEnd(currentBoard,data.pos,data.dropColor);
         if(checkFlag){
@@ -182,6 +199,88 @@ var listBoard=new Map();
       return returnNum;
   }
 
+  function checkThree(currentBoard,dropPos,dropColor){
+
+    let lineCount=0;
+    for(let i=0; i<8; i=i+2){
+      let sum=1;
+      let data1=checkThreeLine(currentBoard,dropPos,dropColor,i);
+      let data2=checkThreeLine(currentBoard,dropPos,dropColor,i+1);
+      sum=sum+data1.num+data2.num;
+      console.log("33 line sum::"+sum);
+      if(sum==3 && (data1.flag && data2.flag)){
+        lineCount++;
+      }
+    }
+
+    if(lineCount==2){
+      return true;
+    }else{
+      return false;
+    }
+
+  }
+
+  function checkThreeLine(currentBoard,dropPos,dropColor,direction){
+      let returnFlag=false;
+      let returnNum=0;
+      let currentX=dropPos.x;
+      let currentY=dropPos.y;
+      let currentIndex=0;
+
+      do{
+        switch(direction){
+          case 0:
+            currentX++;
+            break;
+          case 1:
+            currentX--;
+            break;
+          case 2:
+            currentY++;
+            break;
+          case 3:
+            currentY--;
+              break;
+          case 4:
+            currentX++;
+            currentY++;
+              break;
+          case 5:
+            currentX--;
+            currentY--;
+              break;
+          case 6:
+            currentX++;
+            currentY--;
+              break;
+          case 7:
+            currentX--;
+            currentY++;
+              break;
+        }
+
+        if(currentX>14 || currentX<0 || currentY>14 || currentY<0 ){
+          break;
+        }
+
+        currentIndex=currentX+currentY*15;
+        let currentColor=currentBoard[currentIndex];
+
+        if(currentColor==dropColor){
+          returnNum++;
+        }else if(currentColor==0){
+          returnFlag=true;
+          break;
+        }else{
+          returnFlag=false;
+          break;
+        }
+
+      }while(true);
+
+      return {"flag":returnFlag,"num":returnNum};
+  }
 
 
 //////////////////testCode
